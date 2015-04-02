edf47607e57ec793723c9bb44b049d6afca3866e
Toxa Kniotee <toxa_kniotee@outlook.com>
2015-04-02 15:03:41 -0600

    structural/bridge

--- app/controllers/structural_controller.rb
+++ app/controllers/structural_controller.rb
@@ -3,6 +3,13 @@ class StructuralController < ApplicationController
         @rectangle = Rectangle.new(0, 0, 10, 20)
         @area = @rectangle.area
     end
+
+    def bridge
+        @book1 = BridgeBookAuthorTitle.new('Larry Truett', 'Ruby for Cats', 'CAPS')
+        @book2 = BridgeBookAuthorTitle.new('Larry Truett', 'Ruby for Cats', 'STARS')
+        @book3 = BridgeBookTitleAuthor.new('Larry Truett', 'Ruby for Cats', 'CAPS')
+        @book4 = BridgeBookTitleAuthor.new('Larry Truett', 'Ruby for Cats', 'STARS')
+    end
 end
 
 ##############################################################################################
@@ -28,5 +35,63 @@ class Rectangle
 
     def area
         @legacy_rectangle.old_area
+    end
+end
+
+##############################################################################################
+# Bridge Classes>
+##############################################################################################
+class BridgeBook
+    def initialize(author, title, choice)
+        @author = author
+        @title = author
+        if 'STARS' == choice
+            @bb_imp = BridgeBookStarsImp.new
+        else
+            @bb_imp = BridgeBookCapsImp.new
+        end
+    end
+
+    def show_author
+        @bb_imp.show_author(@author)
+    end
+
+    def show_title
+        @bb_imp.show_title(@title)
+    end
+end
+
+class BridgeBookAuthorTitle < BridgeBook
+    def show_author_title
+        "#{show_author} 's #{show_title}"
+    end
+end
+ 
+class BridgeBookTitleAuthor < BridgeBook
+    def show_title_author
+        "#{show_title} by #{show_author}"
+    end
+end
+ 
+class BridgeBookImp
+end
+
+class BridgeBookCapsImp < BridgeBookImp
+    def show_author(author_in)
+        author_in.upcase
+    end
+
+    def show_title(title_in)
+        title_in.upcase
+    end
+end
+
+class BridgeBookStarsImp < BridgeBookImp
+    def show_author(author_in)
+        author_in.tr(' ', '*')
+    end
+
+    def show_title(title_in)
+        title_in.tr(' ', '*')
     end
 end
