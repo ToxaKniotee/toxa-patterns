1fb09c8b0d13b3aa6fe260d28bbcc30c9e6f62d6
Toxa Kniotee <toxa_kniotee@outlook.com>
2015-05-09 21:28:18 -0500

    Change tab size to 2 spaces and added composite pattern

--- app/controllers/creational_controller.rb
+++ app/controllers/creational_controller.rb
@@ -1,120 +1,120 @@
 class CreationalController < ApplicationController
-    def builder
-        @cook = Cook.new;
-        @hawaiian_pizza_builder = HawaiianPizzaBuilder.new;
-        @spicy_pizza_builder   = SpicyPizzaBuilder.new;
-        @pizzas = Array.new
+  def builder
+    @cook = Cook.new;
+    @hawaiian_pizza_builder = HawaiianPizzaBuilder.new;
+    @spicy_pizza_builder   = SpicyPizzaBuilder.new;
+    @pizzas = Array.new
 
-        @cook.set_pizza_builder(@hawaiian_pizza_builder);
-        @cook.construct_pizza();
+    @cook.set_pizza_builder(@hawaiian_pizza_builder);
+    @cook.construct_pizza();
 
-        @hawaiian = @cook.get_pizza();
-        @pizzas << @hawaiian.open();
+    @hawaiian = @cook.get_pizza();
+    @pizzas << @hawaiian.open();
 
-        @cook.set_pizza_builder(@spicy_pizza_builder);
-        @cook.construct_pizza();
+    @cook.set_pizza_builder(@spicy_pizza_builder);
+    @cook.construct_pizza();
 
-        @spicy = @cook.get_pizza();
-        @pizzas << @spicy.open();
+    @spicy = @cook.get_pizza();
+    @pizzas << @spicy.open();
+  end
+
+  def factory
+    @factory = ComputerFactory.new
+    @laptop = @factory.get_computer('Laptop')
+    @desktop = @factory.get_computer('Desktop')
+  end
+
+  def prototype
+    # Agregamos el nuevo valor agregado por el usuario
+    @role = Factory::make_stooge(params[:choice].to_i)
+  end
+
+  def singleton
+    # Creamos dos clases para ver si resultan ser iguales
+    a, b = Klass.instance, Klass.instance
+    @result = a == b
+    begin
+      Klass.new
+    rescue
+      @error = $!
     end
-
-    def factory
-        @factory = ComputerFactory.new
-        @laptop = @factory.get_computer('Laptop')
-        @desktop = @factory.get_computer('Desktop')
-    end
-
-    def prototype
-        # Agregamos el nuevo valor agregado por el usuario
-        @role = Factory::make_stooge(params[:choice].to_i)
-    end
-
-    def singleton
-        # Creamos dos clases para ver si resultan ser iguales
-        a, b = Klass.instance, Klass.instance
-        @result = a == b
-        begin
-            Klass.new
-        rescue
-            @error = $!
-        end
-    end
+  end
 end
 
 ##############################################################################################
 # Builder classes:
 ##############################################################################################
 class Pizza
-    def set_dough(dough)
-        @dough = dough
-    end
+  def set_dough(dough)
+    @dough = dough
+  end
 
-    def set_sauce(sauce)
-        @sauce = sauce
-    end
+  def set_sauce(sauce)
+    @sauce = sauce
+  end
 
-    def set_topping(topping)
-        @topping = topping
-    end
+  def set_topping(topping)
+    @topping = topping
+  end
 
-    def open
-        "Pizza with #{@dough} dough, #{@sauce} sauce and #{@topping} topping."
-    end
+  def open
+    "Pizza with #{@dough} dough, #{@sauce} sauce and #{@topping} topping."
+  end
 end
 
 class PizzaBuilder
-    def get_pizza
-        @pizza
-    end
+  def get_pizza
+    @pizza
+  end
 
-    def create_new_pizza
-        @pizza = Pizza.new
-    end
+  def create_new_pizza
+    @pizza = Pizza.new
+  end
 end
 
 class HawaiianPizzaBuilder < PizzaBuilder
-    def build_dough
-        @pizza.set_dough('cross')
-    end
+  def build_dough
+    @pizza.set_dough('cross')
+  end
 
-    def build_sauce
-        @pizza.set_sauce('mild')
-    end
+  def build_sauce
+    @pizza.set_sauce('mild')
+  end
 
-    def build_topping
-        @pizza.set_topping('ham + pinneaple')
-    end
+  def build_topping
+    @pizza.set_topping('ham + pinneaple')
+  end
 end
 
 class SpicyPizzaBuilder < PizzaBuilder
-    def build_dough
-        @pizza.set_dough('pan baked')
-    end
+  def build_dough
+    @pizza.set_dough('pan baked')
+  end
 
-    def build_sauce
-        @pizza.set_sauce('hot')
-    end
+  def build_sauce
+    @pizza.set_sauce('hot')
+  end
 
-    def build_topping
-        @pizza.set_topping('pepperoni + salami')
-    end
+  def build_topping
+    @pizza.set_topping('pepperoni + salami')
+  end
 end
 
 class Cook
-    def set_pizza_builder(pb)
-        @pizza_builder = pb
-    end
+  def set_pizza_builder(pb)
+    @pizza_builder = pb
+  end
 
-    def get_pizza
-        @pizza_builder.get_pizza()
-    end
+  def get_pizza
+    @pizza_builder.get_pizza()
+  end
 
-    def construct_pizza
-        @pizza_builder.create_new_pizza()
-        @pizza_builder.build_dough()
-        @pizza_builder.build_sauce()
-        @pizza_builder.build_topping()
-    end
+  def construct_pizza
+    @pizza_builder.create_new_pizza()
+    @pizza_builder.build_dough()
+    @pizza_builder.build_sauce()
+    @pizza_builder.build_topping()
+  end
 end
 
 ##############################################################################################
@@ -124,25 +124,25 @@ class Computer
 end
 
 class Laptop < Computer
-    def type
-        'Laptop'
-    end
+  def type
+    'Laptop'
+  end
 end
 
 class Desktop < Computer
-    def type
-        'Desktop'
-    end
+  def type
+    'Desktop'
+  end
 end
 
 class ComputerFactory
-    def get_computer(type)
-        if type == "Laptop"
-            Laptop.new
-        elsif type == "Desktop"
-            Desktop.new
-        end
+  def get_computer(type)
+    if type == "Laptop"
+      Laptop.new
+    elsif type == "Desktop"
+      Desktop.new
     end
+  end
 end
 
 ##############################################################################################
@@ -152,48 +152,48 @@ class Stooge
 end
 
 class Larry < Stooge
-    def clone
-        Larry.new
-    end
+  def clone
+    Larry.new
+  end
 
-    def slap_stick
-        'Larry: poke eyes'
-    end
+  def slap_stick
+    'Larry: poke eyes'
+  end
 end
 
 class Moe < Stooge
-    def clone
-        Moe.new
-    end
+  def clone
+    Moe.new
+  end
 
-    def slap_stick
-        'Moe: slap head'
-    end
+  def slap_stick
+    'Moe: slap head'
+  end
 end
 
 class Curly < Stooge
-    def clone
-        Curly.new
-    end
+  def clone
+    Curly.new
+  end
 
-    def slap_stick
-        'Curly: suffer abuse'
-    end
+  def slap_stick
+    'Curly: suffer abuse'
+  end
 end
 
 class Factory
-    @@s_prototypes = [0, Larry.new, Moe.new, Curly.new]
+  @@s_prototypes = [0, Larry.new, Moe.new, Curly.new]
 
-    def self.make_stooge(choice)
-        if choice != nil and choice != 0
-            @@s_prototypes[choice].clone
-        end
+  def self.make_stooge(choice)
+    if choice != nil and choice != 0
+      @@s_prototypes[choice].clone
     end
+  end
 end
 
 ##############################################################################################
 # Singleton classes:
 ##############################################################################################
 class Klass
-    include Singleton
+  include Singleton
 end
--- app/controllers/structural_controller.rb
+++ app/controllers/structural_controller.rb
@@ -1,97 +1,158 @@
 class StructuralController < ApplicationController
-    def adapter
-        @rectangle = Rectangle.new(0, 0, 10, 20)
-        @area = @rectangle.area
-    end
+  def adapter
+    @rectangle = Rectangle.new(0, 0, 10, 20)
+    @area = @rectangle.area
+  end
 
-    def bridge
-        @book1 = BridgeBookAuthorTitle.new('Larry Truett', 'Ruby for Cats', 'CAPS')
-        @book2 = BridgeBookAuthorTitle.new('Larry Truett', 'Ruby for Cats', 'STARS')
-        @book3 = BridgeBookTitleAuthor.new('Larry Truett', 'Ruby for Cats', 'CAPS')
-        @book4 = BridgeBookTitleAuthor.new('Larry Truett', 'Ruby for Cats', 'STARS')
-    end
+  def bridge
+    @book1 = BridgeBookAuthorTitle.new('Larry Truett', 'Ruby for Cats', 'CAPS')
+    @book2 = BridgeBookAuthorTitle.new('Larry Truett', 'Ruby for Cats', 'STARS')
+    @book3 = BridgeBookTitleAuthor.new('Larry Truett', 'Ruby for Cats', 'CAPS')
+    @book4 = BridgeBookTitleAuthor.new('Larry Truett', 'Ruby for Cats', 'STARS')
+  end
+
+  def composite
+    @file1 = File.new("readme")
+    @file2 = File.new("license")
+    @file3 = File.new("a.out")
+
+    folder1 = Folder.new("Sub1");
+    folder2 = Folder.new("Sub2");
+    folder3 = Folder.new("Sub3");
+
+    @folder1.addFile(@file1)
+    @folder2.addFile(@file2)
+    @folder3.addFile(@file3)
+    @folder2.addFile(@subFolder1)
+    @folder3.addFile(@subFolder2)
+  end
 end
 
 ##############################################################################################
 # Adapter classes:
 ##############################################################################################
 class LegacyRectangle
-    def initialize(x1, y1, x2, y2)
-        @x1 = x1
-        @y1 = y1
-        @x2 = x2
-        @y2 = y2
-    end
+  def initialize(x1, y1, x2, y2)
+    @x1 = x1
+    @y1 = y1
+    @x2 = x2
+    @y2 = y2
+  end
 
-    def old_area
-        (@x2 - @x1) * (@y2 - @y1)
-    end
+  def old_area
+    (@x2 - @x1) * (@y2 - @y1)
+  end
 end
 
 class Rectangle
-    def initialize(x, y, w, h)
-        @legacy_rectangle = LegacyRectangle.new(x, y, x + w, y + h)
-    end
+  def initialize(x, y, w, h)
+    @legacy_rectangle = LegacyRectangle.new(x, y, x + w, y + h)
+  end
 
-    def area
-        @legacy_rectangle.old_area
-    end
+  def area
+    @legacy_rectangle.old_area
+  end
 end
 
 ##############################################################################################
 # Bridge Classes>
 ##############################################################################################
 class BridgeBook
-    def initialize(author, title, choice)
-        @author = author
-        @title = author
-        if 'STARS' == choice
-            @bb_imp = BridgeBookStarsImp.new
-        else
-            @bb_imp = BridgeBookCapsImp.new
-        end
+  def initialize(author, title, choice)
+    @author = author
+    @title = author
+    if 'STARS' == choice
+      @bb_imp = BridgeBookStarsImp.new
+    else
+      @bb_imp = BridgeBookCapsImp.new
     end
+  end
 
-    def show_author
-        @bb_imp.show_author(@author)
-    end
+  def show_author
+    @bb_imp.show_author(@author)
+  end
 
-    def show_title
-        @bb_imp.show_title(@title)
-    end
+  def show_title
+    @bb_imp.show_title(@title)
+  end
 end
 
 class BridgeBookAuthorTitle < BridgeBook
-    def show_author_title
-        "#{show_author} 's #{show_title}"
-    end
+  def show_author_title
+    "#{show_author} 's #{show_title}"
+  end
 end
- 
+
 class BridgeBookTitleAuthor < BridgeBook
-    def show_title_author
-        "#{show_title} by #{show_author}"
-    end
+  def show_title_author
+    "#{show_title} by #{show_author}"
+  end
 end
- 
+
 class BridgeBookImp
 end
 
 class BridgeBookCapsImp < BridgeBookImp
-    def show_author(author_in)
-        author_in.upcase
-    end
+  def show_author(author_in)
+    author_in.upcase
+  end
 
-    def show_title(title_in)
-        title_in.upcase
-    end
+  def show_title(title_in)
+    title_in.upcase
+  end
 end
 
 class BridgeBookStarsImp < BridgeBookImp
-    def show_author(author_in)
-        author_in.tr(' ', '*')
-    end
+  def show_author(author_in)
+    author_in.tr(' ', '*')
+  end
 
-    def show_title(title_in)
-        title_in.tr(' ', '*')
-    end
+  def show_title(title_in)
+    title_in.tr(' ', '*')
+  end
+end
+
+##############################################################################################
+# Composite clases:
+##############################################################################################
+
+class Item
+  @name = nil
+  @size = 0
+
+  def initialize(name)
+    @name = name
+  end
+
+  def name()
+    return @name
+  end
+
+  def size()
+    return @size
+  end
+end
+
+class File < item
+  def initialize(name)
+    @name = name
+    @size = 1
+  end
+end
+
+class Folder < Item
+  @files = []
+
+  def initialize(name)
+    @name = name
+    @files = []
+  end
+
+  def add_file(file)
+    @files << file
+  end
+
+  def size()
+    return @files.inject(0) { |res, elm| res + elm.size() }
+  end
 end
